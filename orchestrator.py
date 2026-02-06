f4fb7dd67b99dcdea02ca686e686524def3e9ff0
jdpatilonline <jdpatilonline@gmail.com>
2026-02-06 22:46:25 +0530

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -10,34 +10,41 @@ from agents.nmap_agent import run as nmap
 from agents.ai_analysis_agent import run as ai_analysis
 from agents.policy_agent import run as policy
 
-
 def main():
 
+    print("==========================================")
     print("Starting Security Orchestrator")
-    
+
+    print("==========================================")
     print("enterprise_debug is running")
     run_enterprise_debug()
-    
+
+    print("==========================================")
     print("ZAP is running")
     zap()
-    
+
+    print("==========================================")
     print("Semgrep is running")
     semgrep()
-    
+
+    print("==========================================")
     print("Trufflehog is running")
     trufflehog()
-    
+
+    print("==========================================")
     print("Nmap is running")
     nmap()
-    
+
+    print("==========================================")
     print("ai_analysis is running")
     ai_analysis()
-    
+
+    print("==========================================")
     print("Policy Agent is running")
     policy()
 
+    print("==========================================")
     print("Pipeline Completed")
-
 
 if __name__ == "__main__":
     main()
